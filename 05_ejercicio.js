855abb430b7ef26255c229f146f8fa3359a4ad1e
Álvaro <17alma41@gmail.com>
2023-10-21 13:44:08 +0200

    ejercicio 5: area de un circulo

---
+++ 05_ejercicio.js
@@ -0,0 +1,7 @@
+function areaCirculo(r){
+    let resultado = Math.PI * Math.sqrt(r) 
+    return "El resultado del area del circulo es: " + resultado
+}
+
+var operacion = areaCirculo(8);
+console.log(operacion);
