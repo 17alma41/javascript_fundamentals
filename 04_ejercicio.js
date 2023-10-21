19fefd20b80910aa1b6916d2ca9d0a882d0dd936
Álvaro <17alma41@gmail.com>
2023-10-21 12:52:21 +0200

    ejercicio 4: varias operaciones

---
+++ 04_ejercicio.js
@@ -0,0 +1,46 @@
+function suma(a, b){
+    let resultado = a + b
+ return resultado
+ };
+ 
+ var operacion = suma(7, 9);
+ console.log(operacion);
+ 
+ 
+ function resta(a, b){
+     let resultado = a - b
+     return resultado
+ };
+ 
+ var operacion= resta(35, 17);
+ console.log(operacion);
+ 
+ 
+ function multiplicacion(a, b){
+     let resultado = a * b
+     return resultado
+ };
+ 
+ var operacion= multiplicacion(21, 4);
+ console.log(operacion);
+ 
+ 
+ function division(a, b){
+     let resultado = a / b 
+     return resultado
+ };
+ 
+ var operacion= division(30, 2);
+ console.log(operacion);
+ 
+ 
+ function doble(a){
+     let resultado = a*2
+     return resultado
+ };
+ 
+ var operacion= doble(5);
+ console.log(operacion);
+ 
+ 
+ 
