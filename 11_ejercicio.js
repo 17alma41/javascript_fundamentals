5d21e7346497b8035800052fe6e6395bfd1bafd4
Alvaro <17alma41@gmail.com>
2023-10-26 15:18:40 +0200

    ejercicio 11: array

---
+++ 11_ejercicio.js
@@ -0,0 +1,15 @@
+let lista= [2, 2.2, "hola", false]  
+
+mostrarLista(lista)
+
+function mostrarLista(lista) {
+    for (let i = 0; i < lista.length; i++) {
+       console.log(lista[i]) 
+    }
+}
+
+
+
+
+
+
