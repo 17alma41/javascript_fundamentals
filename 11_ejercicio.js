650c44d72014af8487f247f2f9dc34c11a67185b
Alvaro <17alma41@gmail.com>
2023-10-31 14:33:39 +0100

    ejercicio 11: mostrar Array

---
+++ 11_ejercicio.js
@@ -0,0 +1,9 @@
+let lista = [2, 2.3, "Hola", false]
+function mostrarArray(lista) {
+    for (let i = 0; i < lista.length; i++) {
+        const element = lista[i];
+        console.log(mostrarArray)
+    }
+    
+}
+mostrarArray(lista)
